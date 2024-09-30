53837d92a6e2dd43493e6d9049c3f6084d89f1f8
Dilsha Prathibha <104624902+DilshaPrathibha@users.noreply.github.com>
2024-09-30 11:53:51 +0530

    Added favicon

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/contact.css">
     <title>Contact Us</title>
-    <link rel="icon" href="/favicon.png" type="image/png"> <!--ads favicon-->
+    <link rel="icon" href="/images/favicon.webp" type="image/png">
 </head>
 <body>
     <img src="images/contact-cover" alt="contact cover" id="contact-cover">
