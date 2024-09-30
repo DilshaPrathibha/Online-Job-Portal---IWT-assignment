a08bdce054f4327ee0b75ef982e4ff8c3374e57e
Dilsha Prathibha <104624902+DilshaPrathibha@users.noreply.github.com>
2024-09-30 11:14:10 +0530

    added new files

---
+++ contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/contact.css">
+    <title>Contact Us</title>
+    <link rel="icon" href="/favicon.png" type="image/png"> <!--ads favicon-->
+</head>
+<body>
+    <img src="images/contact-cover" alt="contact cover" id="contact-cover">
+    <div class="contact-forum">
+        <form>
+            <input type="text" name="name" placeholder="Name" required> <br>
+            <input type="email" name="email" placeholder="Email" required> <br>
+            <input type="text" name="phone" placeholder="Phone Number" required> <br>
+            <textarea name="message" placeholder="Your Message..." required></textarea> <br><br>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
