e9260459ccf720f50fce089cc09369afb3bfa50f
Dilsha Prathibha <104624902+DilshaPrathibha@users.noreply.github.com>
2024-10-03 03:52:39 +0530

    Rearranged files

--- c_insert.php
+++ c_insert.php
@@ -7,8 +7,11 @@
         $c_phone = $_POST["cphone"];
         $c_message = $_POST["cmessage"];
 
+        date_default_timezone_set('Asia/Colombo');
+        $datetime = date("Y-m-d H:i:s");
+
         // contact_us - table name
-        $sql = "INSERT INTO contact_us VALUES ('','$c_name', '$c_email', '$c_phone', '$c_message', '', '')"; 
+        $sql = "INSERT INTO contact_us VALUES ('','$c_name', '$c_email', '$c_phone', '$c_message', '$datetime', 'pending')"; 
  
         if ($conn ->query($sql)) {
             echo "Incert Successful";
